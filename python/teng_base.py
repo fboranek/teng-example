f00e07649c34e0728f2ea8a1be1862503cba965b
František Bořánek <frantisek.boranek@firma.seznam.cz>
2019-11-22 15:24:21 +0000

    Extend example

--- python/teng_base.py
+++ python/teng_base.py
@@ -8,9 +8,34 @@ TEMPLATE = """
 <html>
 <?teng include file="common-head.html" ?>
     <body>
-        <?teng frag row?><p>${rnum}
-            <?teng frag col?>${cnum} <?teng endfrag?>
-        </p><?teng endfrag?>
+        <!-- value in root -->
+        <p>${var1}</p>
+        <p>${.var1}</p>
+
+        <!-- value in fragment (dic or list) -->
+        <?teng frag global?>
+            <p>${var2} - ${var3}</p>
+        <?teng endfrag?>
+        <!-- value in fragment (dic) -->
+        <p>${$$global.var2} - ${$$global.var3}</p>
+        <!-- value in fragment (dic) addressed from root -->
+        <p>${$$.global.var2} - ${$$.global.var3}</p>
+
+        <?teng frag row?>
+            <p>
+                <!-- value directly in this fragment -->
+                - ${rnum}
+                <!-- value from sub-framgment -->
+                - ${$$dic.var4}
+                <!-- value from root fragment -->
+                - ${.var1}
+                <!-- value from fragment adresed from root -->
+                - ${$$.global.var2}
+            <br>
+                <!-- only way for list -->
+                <?teng frag col?>${cnum} <?teng endfrag?>
+            </p>
+        <?teng endfrag?>
     </body>
 </html>
 """
@@ -18,9 +43,14 @@ TEMPLATE = """
 
 def createFragment():
     return {
+        'var1': 'val1',
+        'global': {
+            'var2': 'val2',
+            'var3': 3
+        },
         'row': [
-            {'rnum': 'A', 'col': [{'cnum': 1}, {'cnum': 2}]},
-            {'rnum': 'B', 'col': [{'cnum': 1}, {'cnum': 2}]}
+            {'rnum': 'A', 'dic': {'var4': 10}, 'col': [{'cnum': 1}, {'cnum': 2}]},
+            {'rnum': 'B', 'dic': {'var4': 20}, 'col': [{'cnum': 1}, {'cnum': 2}]}
         ]
     }
 
